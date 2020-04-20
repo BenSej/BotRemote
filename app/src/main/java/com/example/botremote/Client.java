fe4a09bd5f4b9f5bcd31458dfa91da0c4a72b519
bensedghi <bsedgh2@illinois.edu>
2020-04-20 16:02:27 -0500

    Finished

--- app/src/main/java/com/example/botremote/Client.java
+++ app/src/main/java/com/example/botremote/Client.java
@@ -2,22 +2,19 @@ package com.example.botremote;
 import java.net.*;
 import java.io.*;
 class Client {
-    private static Socket clientSocket;
+    public static Socket clientSocket = new Socket();
     private static PrintWriter out;
-    private static BufferedReader in;
 
     static void startConnection(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
         out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
     }
     static void sendMessage(String message) throws IOException {
-        out.println(message);
-        in.readLine();
+        if (out != null) {
+            out.println(message);
+        }
     }
-    static void stopConnection() throws IOException{
-        in.close();
-        out.close();
+    static void stopConnection() throws IOException {
         clientSocket.close();
     }
 }
--- app/src/main/java/com/example/botremote/MainActivity.java
+++ app/src/main/java/com/example/botremote/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,23 +15,22 @@ public class MainActivity extends AppCompatActivity {
     private Button connect;
     private Button forward;
     private Button stop;
-    private Button disconnect;
     private Button moveRight;
     private Button moveLeft;
     private Button moveBackward;
     private TextView connectionStatus;
     private char[] connected = "Connected".toCharArray();
-    private char[] disconnected = "Disconnected".toCharArray();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         connect = findViewById(R.id.connectButton);
         forward = findViewById(R.id.fowardButton);
         stop = findViewById(R.id.stopButton);
-        disconnect = findViewById(R.id.disconnectButton);
         connectionStatus = findViewById(R.id.connectionStatus);
         moveRight = findViewById(R.id.rightButton);
         moveLeft = findViewById(R.id.leftButton);
@@ -40,11 +40,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 try {
-                    Client.startConnection("73.50.89.25", 6666);
-                    connectionStatus.setText(connected, 0, connected.length);
-                    connectionStatus.setTextColor(Color.GREEN);
+                    if(Client.clientSocket.isConnected()) {
+                        return;
+                    } else {
+                        Client.startConnection("192.168.1.33", 6197);
+                        connectionStatus.setText(connected, 0, connected.length);
+                        connectionStatus.setTextColor(Color.GREEN);
+                    }
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    System.out.println(e.toString());
                 }
             }
         });
@@ -53,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 try {
-                    Client.sendMessage("forward");
+                    Client.sendMessage("moveForward");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -98,20 +102,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 try {
                     Client.sendMessage("stop");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        disconnect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    Client.stopConnection();
-                    Server.stop();
-                    connectionStatus.setText(disconnected, 0, disconnected.length);
-                    connectionStatus.setTextColor(Color.GRAY);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
