09b77997a3384270b75d9aba8065e98d5e69ee20
BenSej <60634474+BenSej@users.noreply.github.com>
2020-04-26 08:57:01 -0500

    Checkstyle

--- app/src/main/java/com/example/botremote/Bot.java
+++ app/src/main/java/com/example/botremote/Bot.java
@@ -1,20 +1,14 @@
-package com.example.botremote;
-
 import com.pi4j.io.gpio.*;
 import java.net.*;
 import java.io.*;
 
 class Server {
-    private static ServerSocket serverSocket;
-    private static Socket clientSocket;
-    private static BufferedReader in;
-
-    private void start(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
-        clientSocket = serverSocket.accept();
+    private void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(6197);
+        Socket clientSocket = serverSocket.accept();
         System.out.println("Connected");
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String message = "";
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String message;
         while ((message = in.readLine()) != null) {
             switch (message) {
                 case "moveForward":
@@ -45,26 +39,26 @@ class Server {
 
     public static void main(String[] args) throws IOException {
         Server server = new Server();
-        server.start(6197);
+        server.start();
     }
 }
 
-public class Bot {
+class Bot {
     static private boolean stopped = true;
-    static GpioController eOne = GpioFactory.getInstance();
-    static GpioController eTwo = GpioFactory.getInstance();
-    static GpioController iOne = GpioFactory.getInstance();
-    static GpioController iTwo = GpioFactory.getInstance();
-    static GpioController iThree = GpioFactory.getInstance();
-    static GpioController iFour = GpioFactory.getInstance();
-    static GpioPinDigitalOutput enableOne = eOne.provisionDigitalOutputPin(RaspiPin.GPIO_06, "", PinState.LOW);
-    static GpioPinDigitalOutput enableTwo = eTwo.provisionDigitalOutputPin(RaspiPin.GPIO_03, "", PinState.LOW);
-    static GpioPinDigitalOutput inOne = iOne.provisionDigitalOutputPin(RaspiPin.GPIO_04, "", PinState.LOW);
-    static GpioPinDigitalOutput inTwo = iTwo.provisionDigitalOutputPin(RaspiPin.GPIO_05, "", PinState.LOW);
-    static GpioPinDigitalOutput inThree = iThree.provisionDigitalOutputPin(RaspiPin.GPIO_02, "", PinState.LOW);
-    static GpioPinDigitalOutput inFour = iFour.provisionDigitalOutputPin(RaspiPin.GPIO_00, "", PinState.LOW);
+    private static GpioController eOne = GpioFactory.getInstance();
+    private static GpioController eTwo = GpioFactory.getInstance();
+    private static GpioController iOne = GpioFactory.getInstance();
+    private static GpioController iTwo = GpioFactory.getInstance();
+    private static GpioController iThree = GpioFactory.getInstance();
+    private static GpioController iFour = GpioFactory.getInstance();
+    private static GpioPinDigitalOutput enableOne = eOne.provisionDigitalOutputPin(RaspiPin.GPIO_06, "", PinState.LOW);
+    private static GpioPinDigitalOutput enableTwo = eTwo.provisionDigitalOutputPin(RaspiPin.GPIO_03, "", PinState.LOW);
+    private static GpioPinDigitalOutput inOne = iOne.provisionDigitalOutputPin(RaspiPin.GPIO_04, "", PinState.LOW);
+    private static GpioPinDigitalOutput inTwo = iTwo.provisionDigitalOutputPin(RaspiPin.GPIO_05, "", PinState.LOW);
+    private static GpioPinDigitalOutput inThree = iThree.provisionDigitalOutputPin(RaspiPin.GPIO_02, "", PinState.LOW);
+    private static GpioPinDigitalOutput inFour = iFour.provisionDigitalOutputPin(RaspiPin.GPIO_00, "", PinState.LOW);
 
-    public static void moveForward () {
+    static void moveForward () {
         if (stopped) {
             stopped = false;
             inOne.high();
@@ -73,7 +67,7 @@ public class Bot {
             enableTwo.high();
         }
     }
-    public static void stop () {
+    static void stop () {
         inOne.low();
         inTwo.low();
         inThree.low();
@@ -82,7 +76,7 @@ public class Bot {
         enableTwo.low();
         stopped = true;
     }
-    public static void moveRight () {
+    static void moveRight () {
         if (stopped) {
             stopped = false;
             inOne.high();
@@ -91,7 +85,7 @@ public class Bot {
             enableTwo.high();
         }
     }
-    public static void moveLeft () {
+    static void moveLeft () {
         if (stopped) {
             stopped = false;
             inTwo.high();
@@ -100,7 +94,7 @@ public class Bot {
             enableTwo.high();
         }
     }
-    public static void moveBackward () {
+    static void moveBackward () {
         if (stopped) {
             inTwo.high();
             inFour.high();
