4c61d054570066e32fce038ab383d9a94abe5493
BenSej <60634474+BenSej@users.noreply.github.com>
2020-04-25 22:11:10 -0500

    Finished

--- app/src/main/java/com/example/botremote/Bot.java
+++ app/src/main/java/com/example/botremote/Bot.java
@@ -1,34 +1,79 @@
-import com.pi4j.io.gpio.*;
+package com.example.botremote;
 
-class Bot {
-    private boolean stopped = true;
+import com.pi4j.io.gpio.*;
+import java.net.*;
+import java.io.*;
+
+class Server {
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+    private static BufferedReader in;
+
+    private void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        System.out.println("Connected");
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String message = "";
+        while ((message = in.readLine()) != null) {
+            switch (message) {
+                case "moveForward":
+                    Bot.moveForward();
+                    System.out.println("Bot Moving Forwards");
+                    break;
+                case "moveBackward":
+                    Bot.moveBackward();
+                    System.out.println("Bot Moving Backwards");
+                    break;
+                case "moveRight":
+                    Bot.moveRight();
+                    System.out.println("Bot Moving Right");
+                    break;
+                case "moveLeft":
+                    Bot.moveLeft();
+                    System.out.println("Bot Moving Left");
+                    break;
+                case "stop":
+                    Bot.stop();
+                    System.out.println("Bot Stopped");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        server.start(6197);
+    }
+}
+
+public class Bot {
+    static private boolean stopped = true;
     static GpioController eOne = GpioFactory.getInstance();
     static GpioController eTwo = GpioFactory.getInstance();
     static GpioController iOne = GpioFactory.getInstance();
     static GpioController iTwo = GpioFactory.getInstance();
     static GpioController iThree = GpioFactory.getInstance();
     static GpioController iFour = GpioFactory.getInstance();
-    static GpioPinDigitalOutput enableOne = eOne.provisionDigitalOutputPin(RaspiPin.GPIO_04, "enableOne", PinState.LOW);
-    static GpioPinDigitalOutput enableTwo = eTWo.provisionDigitalOutputPin(RaspiPin.GPIO_18, "enableTwo", PinState.LOW);
-    static GpioPinDigitalOutput inOne = iOne.provisionDigitalOutputPin(RaspiPin.GPIO_02, "inOne", PinState.LOW);
-    static GpioPinDigitalOutput inTwo = iTwo.provisionDigitalOutputPin(RaspiPin.GPIO_03, "inTwo", PinState.LOW);
-    static GpioPinDigitalOutput inThree = iThree.provisionDigitalOutputPin(RaspiPin.GPIO_14, "inThree", PinState.LOW);
-    static GpioPinDigitalOutput inFour = iFour.provisionDigitalOutputPin(RaspiPin.GPIO_15, "inFour", PinState.LOW);
+    static GpioPinDigitalOutput enableOne = eOne.provisionDigitalOutputPin(RaspiPin.GPIO_06, "", PinState.LOW);
+    static GpioPinDigitalOutput enableTwo = eTwo.provisionDigitalOutputPin(RaspiPin.GPIO_03, "", PinState.LOW);
+    static GpioPinDigitalOutput inOne = iOne.provisionDigitalOutputPin(RaspiPin.GPIO_04, "", PinState.LOW);
+    static GpioPinDigitalOutput inTwo = iTwo.provisionDigitalOutputPin(RaspiPin.GPIO_05, "", PinState.LOW);
+    static GpioPinDigitalOutput inThree = iThree.provisionDigitalOutputPin(RaspiPin.GPIO_02, "", PinState.LOW);
+    static GpioPinDigitalOutput inFour = iFour.provisionDigitalOutputPin(RaspiPin.GPIO_00, "", PinState.LOW);
 
-    static void moveForward() {
+    public static void moveForward () {
         if (stopped) {
             stopped = false;
-            //configure first motor current for forward movement
             inOne.high();
-            //configure second motor current for forward movement
             inThree.high();
-            //enable both motors
             enableOne.high();
             enableTwo.high();
         }
     }
-    static void stop() {
-        //diable all current flow and disable motors
+    public static void stop () {
         inOne.low();
         inTwo.low();
         inThree.low();
@@ -37,37 +82,28 @@ class Bot {
         enableTwo.low();
         stopped = true;
     }
-    static void moveRight() {
+    public static void moveRight () {
         if (stopped) {
             stopped = false;
-            //configure first motor current for forward movement
             inOne.high();
-            //configure second motor current for backward movement
             inFour.high();
-            //enable both motors
             enableOne.high();
             enableTwo.high();
         }
     }
-    static void moveLeft() {
+    public static void moveLeft () {
         if (stopped) {
             stopped = false;
-            //configure first motor for backward movement
             inTwo.high();
-            //configure second motor for forward movement
             inThree.high();
-            //enable both motors
             enableOne.high();
             enableTwo.high();
         }
     }
-    static void moveBackward() {
+    public static void moveBackward () {
         if (stopped) {
-            //configure first motor current for backward movement
             inTwo.high();
-            //configure second motor current for backward movement
             inFour.high();
-            //enable both motors
             enableOne.high();
             enableTwo.high();
         }
