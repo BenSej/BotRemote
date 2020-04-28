99f2f2a96728c8430a92247afd0fa26b272df261
BenSej <60634474+BenSej@users.noreply.github.com>
2020-04-28 18:09:10 -0500

    Fixed exit when client closed

--- Server.java
+++ Server.java
@@ -14,33 +14,42 @@ class Server {
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         String message;
-        while ((message = in.readLine()) != null) {
-            switch (message) {
-                case "moveForward":
-                    System.out.println("Bot Moving Forwards");
-                    break;
-                case "moveBackward":
-                    System.out.println("Bot Moving Backwards");
-                    break;
-                case "moveRight":
-                    System.out.println("Bot Moving Right");
-                    break;
-                case "moveLeft":
-                    System.out.println("Bot Moving Left");
-                    break;
-                case "stop":
-                    System.out.println("Bot Stopped");
-                    break;
-                default:
-                    break;
-            }
-        }
+        try {
+	        while ((message = in.readLine()) != null) {
+	            switch (message) {
+	                case "moveForward":
+	                    System.out.println("Bot Moving Forwards");
+	                    break;
+	                case "moveBackward":
+	                    System.out.println("Bot Moving Backwards");
+	                    break;
+	                case "moveRight":
+	                    System.out.println("Bot Moving Right");
+	                    break;
+	                case "moveLeft":
+	                    System.out.println("Bot Moving Left");
+	                    break;
+	                case "stop":
+	                    System.out.println("Bot Stopped");
+	                    break;
+	                default:
+	                    break;
+	            }
+	        }
+	        System.out.println("client disconnected");
+	        stop();
+	        start();
+	    } catch (IOException e) {
+	    	System.out.println("disconnected");
+	    	stop();
+	    	start();
+	    }
     }
     public static void stop() throws IOException {
-        serverSocket.close();
-        clientSocket.close();
+    	serverSocket.close();
+    	clientSocket.close();
     }
     public static void main(String[] args) throws IOException {
-        start();
+    	start();
     }
 }
