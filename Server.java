98f282e13b80c7330c988ce7d6e67ec7734c9101
BenSej <60634474+BenSej@users.noreply.github.com>
2020-04-27 21:20:43 -0500

    For tests

---
+++ Server.java
@@ -0,0 +1,46 @@
+import java.net.*;
+import java.io.*;
+
+class Server {
+    public static Server server = new Server();
+    public static ServerSocket serverSocket;
+    public static Socket clientSocket;
+
+    public static void start() throws IOException {
+        serverSocket = new ServerSocket(6197);
+        System.out.println("Server listening on port 6197");
+        clientSocket = serverSocket.accept();
+        System.out.println("Connected");
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        String message;
+        while ((message = in.readLine()) != null) {
+            switch (message) {
+                case "moveForward":
+                    System.out.println("Bot Moving Forwards");
+                    break;
+                case "moveBackward":
+                    System.out.println("Bot Moving Backwards");
+                    break;
+                case "moveRight":
+                    System.out.println("Bot Moving Right");
+                    break;
+                case "moveLeft":
+                    System.out.println("Bot Moving Left");
+                    break;
+                case "stop":
+                    System.out.println("Bot Stopped");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    public static void stop() throws IOException {
+        serverSocket.close();
+        clientSocket.close();
+    }
+    public static void main(String[] args) throws IOException {
+        start();
+    }
+}
