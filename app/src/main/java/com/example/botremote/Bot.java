1bcfc6256f961003d7cd119159f29e9e9190a85f
zachcacini <zcacini2@illinois.edu>
2020-05-04 15:46:34 -0500

    record() and other button interactions created

--- app/src/main/java/com/example/botremote/Bot.java
+++ app/src/main/java/com/example/botremote/Bot.java
@@ -18,6 +18,14 @@ class Server {
         try {
             while ((message = in.readLine()) != null) {
                 switch (message) {
+                    case "playRecording":
+                        System.out.println("Playing Recording");
+                        Command.enact(commands);
+                        break;
+                    case "startRecording":
+                        System.out.println("Recording Started");
+                        record();
+                        break;
                     case "moveForward":
                         System.out.println("Bot Moving Forwards");
                         Bot.moveForward();
@@ -51,6 +59,56 @@ class Server {
             start();
         }
     }
+
+    private void record() {
+        serverSocket = new ServerSocket(6197);
+        //System.out.println("Server listening on port 6197");
+        clientSocket = serverSocket.accept();
+        //System.out.println("Connected");
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String message;
+        List<String> recording = new ArrayList<String>();
+        int count = 0;
+        try {
+            while ((message = in.readLine()) != null || count < 10) {
+                switch (message) {
+                    case "endRecording":
+                        recording.add("endRecording");
+                        System.out.println("Recording Ended");
+                        start();
+                        break;
+                    case "moveForward":
+                        recording.add("moveForward");
+                        System.out.println("Bot Moving forward Recorded");
+                        count++;
+                        break;
+                    case "moveBackward":
+                        recording.add("moveBackward");
+                        System.out.println("Bot Moving Backwards");
+                        count++;
+                        break;
+                    case "moveRight":
+                        recording.add("moveRight");
+                        System.out.println("Bot Moving Right Recorded");
+                        count++;
+                        break;
+                    case "moveLeft":
+                        recording.add("moveLeft");
+                        System.out.println("Bot Moving Left Recorded");
+                        count++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            System.out.println("Recording Ended");
+        } catch (IOException e) {
+            System.out.println("Recording Error: Bot Disconnected");
+            stop();
+            start();
+        }
+    }
+
     private static void stop() throws IOException {
         serverSocket.close();
         clientSocket.close();
@@ -106,37 +164,41 @@ class Bot {
     }
 }
 public class Command {
-    private List<String> commands = new ArrayList<String>();
+    private static List<String> commands = new ArrayList<String>();
 
     public Programmed(List<String> input) {
-        for (int counter = 0; counter < input.size(); counter++) {
-            commands.add(input.get(counter), 0);
-        }
+        commands = input;
     }
 
-    public void enact(List<String> input) {
+    public static void enact(List<String> input) {
         for (int counter = 0; counter < 10; counter++) {
             if (input.get(counter).equals("moveForward")) {
+                System.out.println("Bot Moving Forward from Recording");
                 Bot.moveForward();
                 wait(2000);
                 Bot.stop();
             } else if (input.get(counter).equals("moveBackward")) {
+                System.out.println("Bot Moving Backward from Recording");
                 Bot.moveBackward();
                 wait(2000);
                 Bot.stop();
             } else if (input.get(counter).equals("moveLeft")) {
+                System.out.println("Bot Moving Left from Recording");
                 Bot.moveLeft();
                 wait(2000);
                 Bot.stop();
             } else if (input.get(counter).equals("moveRight")) {
+                System.out.println("Bot Moving Right from Recording");
                 Bot.moveRight();
                 wait(2000);
                 Bot.stop();
             } else {
+                System.out.println("Bot Stopped from Recording");
                 Bot.stop();
                 return
             }
         }
+        System.out.println("End of Recording")
         Bot.stop();
         return;
     }
